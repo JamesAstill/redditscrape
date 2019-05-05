9b5feacc6dc4df4d376288080d81327bb030742e
James Astill <james.astill.22@gmail.com>
2019-05-05 23:10:48 +0100

    Changed to use requests over urllib3. Initial match list changed to dictionary to improve presentation

--- scraper.py
+++ scraper.py
@@ -1,9 +1,7 @@
 from bs4 import BeautifulSoup
 import re
-import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+import requests
 
-http = urllib3.PoolManager()
 #Set up the current subreddit name
 current_reddit = 'redditsoccercity'
 url = 'https://www.reddit.com/r/' + current_reddit + '/'
@@ -11,19 +9,20 @@ url = 'https://www.reddit.com/r/' + current_reddit + '/'
 #Function to pool the subreddit for open threads
 def getMatchesReddit():
 	#Http request from url above
-	response = http.request('GET', url)
+	response = requests.get(url, headers={'User-agent': 'Mozilla/5.0'}).content
 	#Parse HTTP with html parser
-	soup = BeautifulSoup(response.data, 'html.parser')
+	soup = BeautifulSoup(response, 'html.parser')
 	#Links are located within anchor <a> tags
 	out = soup.find_all("a")
-	match_links = []
+	match_links = {}
 	#Iterate through all links to find those ending hhmm_gmt_matchname
 	for link in out:
 		temp = link.get('href')
 		words = temp.split('/')
-		if len(words) == 9:
-			if re.match(r"[0-9]",words[7]):
-				match_links.append(words[6]+"/"+words[7])
+		if len(words) == 7:
+			if re.match(r"[0-9]",words[5]): 
+				if re.match(r"\[\d",link.get_text()):
+					match_links[link.get_text()] = (words[4]+"/"+words[5])
 	return match_links
 
 #Call the function to allow matches to be iterable
@@ -31,17 +30,17 @@ match_urls = getMatchesReddit()
 user_menu = []
 i=1
 ### USER MENU - Print available matches, listed 1 to N ###
-for match in match_urls:
-	url2 = url + 'comments/' + match
+for key, value in match_urls.items():
+	url2 = url + 'comments/' + match_urls[key]
 	user_menu.append(url2)
-	print(str(i) + ". " + url)
+	print(str(i) + ". " + key)
 	i=i+1
 user_choice = input("Which match would you like links for?\n")
 url_chosen = url + 'comments/' + match_urls[int(user_choice)-1]
 
 #User choice is then pooled as previously, using HTML parser
-response = http.request('GET', url_chosen)
-soup = BeautifulSoup(response.data, 'html.parser')
+response = requests.get(url_chosen, headers={'User-agent': 'Mozilla/5.0'}).content
+soup = BeautifulSoup(response, 'html.parser')
 
 matches = []
 #Comments are stored within a quoute block which uses a paragraph tag <p>.
